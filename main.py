3fd93d0cf5a9b942566816629b42a1b743f54705
jmaxfield2412 <jmaxfield@student.strode-college.ac.uk>
2020-11-04 09:39:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Characters Problem
+
+#Dictionary
+digits = "0123456789"
+characters = "abcdABCD@#!£"
+
+#Output
+print("The digits are:",digits)
+print("The characters are:",characters)
+print("The alphanumericals are:",digits,"",characters)
